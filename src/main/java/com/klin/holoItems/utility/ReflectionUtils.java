e4789847a06626069592cc1942b2bb137b3983ce
cpaca <37684627+cpaca@users.noreply.github.com>
2023-04-06 19:14:26 -0700

    Prepare the Black Magic
    This is a separate commit to the next one incase I want to undo the next commit but keep the black magic that I've written here.

--- src/main/java/com/klin/holoItems/utility/ReflectionUtils.java
+++ src/main/java/com/klin/holoItems/utility/ReflectionUtils.java
@@ -7,8 +7,8 @@ import org.bukkit.entity.LivingEntity;
 import org.bukkit.inventory.ItemStack;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.Arrays;
 
 public class ReflectionUtils {
 
@@ -20,9 +20,11 @@ public class ReflectionUtils {
     private static Field repairCost;
 
     private static Method NMSifyItemStack;
+    private static Method NMSItemStackGetItem;
     private static Class<?> craftWorldClass;
     private static Method getWorldHandleMethod;
-    private static Class<?> craftBukkitLivingEntityClass;
+    private static Class<?> worldClass;
+    private static Class<?> craftEntityClass;
     private static Method getLivingEntityHandleMethod;
     private static Class<?> minecraftEntityLivingClass;
     private static Method finishUsingItemMethod;
@@ -37,6 +39,9 @@ public class ReflectionUtils {
             final Class<?> craftItemStackClass = Class.forName(craft + ".inventory.CraftItemStack", false, Bukkit.getServer().getClass().getClassLoader());
             // asNMSCopy: Converts from Bukkit ItemStack to net.minecraft.world.item.ItemStack
             NMSifyItemStack = craftItemStackClass.getMethod("asNMSCopy", ItemStack.class);
+            // We'll also need the Item object from the NMS ItemStack, so get that too.
+            final Class<?> NMSItemStack = NMSifyItemStack.getReturnType();
+            NMSItemStackGetItem = NMSItemStack.getMethod("c"); // getItem
 
             // According to this bukkit.org forum link:
             // https://bukkit.org/threads/cast-org-bukkit-world-to-net-minecraft-server-v1_8_r3-world.431984/
@@ -46,21 +51,19 @@ public class ReflectionUtils {
             // System.out.println("Is Bukkit world a superclass of craftWorld? " + World.class.isAssignableFrom(craftWorldClass));
             // This gives a NMS.level.WorldServer class
             getWorldHandleMethod = craftWorldClass.getMethod("getHandle");
-
             // worldClass is a superclass of NMS.level.WorldServer
             // so you can cast from NMS.level.worldServer to worldClass
-            Class<?> worldClass = Class.forName("net.minecraft.world.level.World", false, Bukkit.getServer().getClass().getClassLoader());
-            // proof that worldClass is a superclass if NMS.level.worldServer
+            worldClass = Class.forName("net.minecraft.world.level.World", false, Bukkit.getServer().getClass().getClassLoader());
+            // proof that worldClass is a superclass of NMS.level.worldServer
             // System.out.println("Can you cast from getWorldHandleMethod returnType to worldClass?" + worldClass.isAssignableFrom(getWorldHandleMethod.getReturnType()));
 
             // I can't find my notes on how to convert from Bukkit entity to CraftEntity?
             // And google isn't being particularly helpful, except giving me something which I have no idea if it'll work.
             // Apparantly I should just cast from Bukkit LivingEntity to CraftEntity and ... well, it'll work
             // So craftBukkitLivingEntityClass does the conversion
-            craftBukkitLivingEntityClass = Class.forName(craft + ".entity.CraftEntity");
+            craftEntityClass = Class.forName(craft + ".entity.CraftEntity");
             // getHandle can get a net.minecraft.world.entity.Entity
-            getLivingEntityHandleMethod = craftBukkitLivingEntityClass.getMethod("getHandle");
-
+            getLivingEntityHandleMethod = craftEntityClass.getMethod("getHandle");
             // And then I just have to pray that we can cast THAT to EntityLiving
             minecraftEntityLivingClass = Class.forName("net.minecraft.world.entity.EntityLiving");
 
@@ -69,7 +72,7 @@ public class ReflectionUtils {
             //System.out.println(getWorldHandleMethod.getReturnType());
             System.out.println(worldClass);
             System.out.println(minecraftEntityLivingClass);
-            finishUsingItemMethod = itemClass.getMethod("a", NMSifyItemStack.getReturnType(), worldClass, minecraftEntityLivingClass);
+            finishUsingItemMethod = itemClass.getMethod("a", NMSItemStack, worldClass, minecraftEntityLivingClass);
             System.out.println("finishUsingItemMethod: " + finishUsingItemMethod);
 
             setup = true;
@@ -100,6 +103,38 @@ public class ReflectionUtils {
     public static ItemStack finishUsingItem(ItemStack item, World world, LivingEntity entity){
         if(!setup){
             setup();
+        }
+
+        try {
+            // Convert from Bukkit ItemStack to NMS ItemStack
+            Object NMSItemStack = NMSifyItemStack.invoke(null, item);
+            // While we're here, get the Item class too.
+            Object NMSItemObject = NMSItemStackGetItem.invoke(NMSItemStack);
+
+            // Convert from Bukkit World to net.minecraft.world.level.World (aka NMSWorld or worldClass)
+            // This is done by the following conversion-chain:
+            // Bukkit World -> CraftWorld -> NMS.level.worldServer -> NMSWorld
+            // Bukkit World -> CraftWorld
+            Object craftworldObject = craftWorldClass.cast(world);
+            // CraftWorld -> NMS.level.worldServer
+            Object worldServer = getWorldHandleMethod.invoke(craftworldObject);
+            // NMS.level.worldServer -> NMSWorld
+            Object NMSWorld = worldClass.cast(worldServer);
+
+            // Convert from Bukkit entity to EntityLiving
+            // This is done by the following conversion-chain:
+            // Bukkit Entity -> CraftEntity -> NMS entity.Entity -> NMS EntityLiving
+            // Bukkit Entity -> CraftEntity
+            Object craftEntity = craftEntityClass.cast(entity);
+            // CraftEntity -> NMS entity.Entity
+            Object NMSEntity = getLivingEntityHandleMethod.invoke(craftEntity);
+            // NMS entity.Entity -> NMS EntityLiving
+            Object NMSEntityLiving = minecraftEntityLivingClass.cast(NMSEntity);
+
+            // Now that everything is the right type, call finishUsingItem.
+            finishUsingItemMethod.invoke(NMSItemObject, NMSItemStack, NMSWorld, NMSEntityLiving);
+        } catch (IllegalAccessException | InvocationTargetException e) {
+            e.printStackTrace();
         }
 
         return null;
