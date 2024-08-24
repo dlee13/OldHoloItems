75be0ab9901a7b994b776c808915b941a097c792
Derek Lee <derek.scott.lee13@gmail.com>
2024-08-24 01:13:06 -0700

    Rework ShoulderBagStrap

--- src/main/java/com/klin/holoItems/collections/misc/achanCollection/items/ShoulderBagStrap.java
+++ src/main/java/com/klin/holoItems/collections/misc/achanCollection/items/ShoulderBagStrap.java
@@ -1,67 +1,103 @@
 package com.klin.holoItems.collections.misc.achanCollection.items;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
+import org.bukkit.block.ShulkerBox;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.inventory.InventoryCloseEvent;
+import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.InventoryHolder;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.ShapedRecipe;
+import org.bukkit.inventory.meta.BlockStateMeta;
+import org.jetbrains.annotations.NotNull;
+
 import com.klin.holoItems.HoloItems;
 import com.klin.holoItems.Item;
 import com.klin.holoItems.interfaces.Closeable;
 import com.klin.holoItems.interfaces.Holdable;
 import com.klin.holoItems.interfaces.Placeable;
-import com.klin.holoItems.utility.Utility;
-import org.bukkit.Bukkit;
-import org.bukkit.Location;
-import org.bukkit.Material;
-import org.bukkit.NamespacedKey;
-import org.bukkit.block.Block;
-import org.bukkit.block.BlockState;
-import org.bukkit.block.ShulkerBox;
-import org.bukkit.event.block.BlockPlaceEvent;
-import org.bukkit.event.inventory.InventoryCloseEvent;
-import org.bukkit.inventory.InventoryHolder;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.ShapedRecipe;
 
 public class ShoulderBagStrap extends Item implements Placeable, Holdable, Closeable {
     public static final String name = "shoulderBagStrap";
 
     private static final Material material = Material.CHAIN;
     private static final int quantity = 1;
-    private static final String lore =
-            "Easy carry shulkers";
+    private static final String lore = "Easy carry shulkers";
     private static final int durability = 0;
     public static final boolean stackable = false;
     private static final boolean shiny = false;
     public static final int cost = 0;
 
-    public ShoulderBagStrap(){
+    public ShoulderBagStrap() {
         super(name, material, quantity, lore, durability, stackable, shiny, cost);
     }
 
-    public void registerRecipes(){
-        ShapedRecipe recipe = new ShapedRecipe(new NamespacedKey(HoloItems.getInstance(), name), item);
-        recipe.shape(" ab","ab "," ab");
+    public void registerRecipes() {
+        final var recipe = new ShapedRecipe(new NamespacedKey(HoloItems.getInstance(), name), item);
+        recipe.shape(" ab", "ab ", " ab");
         recipe.setIngredient('a', Material.IRON_INGOT);
         recipe.setIngredient('b', Material.LEATHER);
         recipe.setGroup(name);
         Bukkit.getServer().addRecipe(recipe);
-
-        Utility.mirror(recipe, name, item);
     }
 
-    public void ability(BlockPlaceEvent event){
-        BlockState state = event.getBlockPlaced().getState();
-        if(state instanceof ShulkerBox)
-            event.getPlayer().openInventory(((ShulkerBox) state).getInventory());
+    @Override
+    public void ability(BlockPlaceEvent event) {
+        final var itemStack = event.getItemInHand();
+        if (itemStack.getItemMeta() instanceof BlockStateMeta blockStateMeta && blockStateMeta.hasBlockState()
+                && blockStateMeta.getBlockState() instanceof ShulkerBox shulkerBox) {
+            event.setCancelled(true);
+            final var shoulderBag = new ShoulderBag(itemStack.getType(), shulkerBox);
+            final var player = event.getPlayer();
+            player.getInventory().removeItem(itemStack);
+            player.openInventory(shoulderBag.getInventory());
+        }
     }
 
-    public void ability(InventoryCloseEvent event){
-        InventoryHolder holder = event.getInventory().getHolder();
-        if(holder instanceof ShulkerBox) {
-            Block block = ((ShulkerBox) holder).getBlock();
-            Location loc = block.getLocation();
-            for(ItemStack item : block.getDrops()) {
-                if (!event.getPlayer().getInventory().addItem(item).isEmpty())
-                    loc.getWorld().dropItemNaturally(loc, item);
+    @Override
+    public void ability(InventoryCloseEvent event) {
+        if (event.getInventory().getHolder() instanceof ShoulderBag shoulderBag) {
+            final var itemStack = shoulderBag.getItemStack();
+            final var player = event.getPlayer();
+            final var excess = player.getInventory().addItem(itemStack);
+            if (!excess.isEmpty()) {
+                player.getWorld().dropItemNaturally(player.getLocation(), itemStack);
             }
-            block.setType(Material.AIR);
+        }
+    }
+
+    private static class ShoulderBag implements InventoryHolder {
+
+        final Material material;
+        final ShulkerBox shulkerBox;
+        final Inventory inventory;
+
+        public ShoulderBag(final Material material, final ShulkerBox shulkerBox) {
+            this.material = material;
+            this.shulkerBox = shulkerBox;
+            final var title = shulkerBox.customName();
+            this.inventory = title == null
+                    ? Bukkit.createInventory(this, InventoryType.SHULKER_BOX)
+                    : Bukkit.createInventory(this, InventoryType.SHULKER_BOX, title);
+            inventory.setContents(shulkerBox.getInventory().getContents());
+        }
+
+        @Override
+        public @NotNull Inventory getInventory() {
+            return inventory;
+        }
+
+        public ItemStack getItemStack() {
+            final var itemStack = new ItemStack(material);
+            if (itemStack.getItemMeta() instanceof BlockStateMeta blockStateMeta) {
+                shulkerBox.getInventory().setContents(inventory.getContents());
+                blockStateMeta.setBlockState(shulkerBox);
+                itemStack.setItemMeta(blockStateMeta);
+            }
+            return itemStack;
         }
     }
 }
