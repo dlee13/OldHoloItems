bce839835ccdaef02811de5c3699d57407f9c850
Derek Lee <derek.scott.lee13@gmail.com>
2024-09-13 18:12:29 -0700

    Refactor sand portal

--- src/main/java/com/klin/holoItems/collections/en1/watsonCollection/items/SandPortal.java
+++ src/main/java/com/klin/holoItems/collections/en1/watsonCollection/items/SandPortal.java
@@ -1,65 +1,69 @@
 package com.klin.holoItems.collections.en1.watsonCollection.items;
 
-import com.klin.holoItems.HoloItems;
-import com.klin.holoItems.Item;
-import com.klin.holoItems.interfaces.Dispensable;
-import com.klin.holoItems.interfaces.Placeable;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
-import org.bukkit.block.Block;
-import org.bukkit.block.BlockFace;
-import org.bukkit.block.BlockState;
 import org.bukkit.block.Container;
 import org.bukkit.block.data.type.Dispenser;
 import org.bukkit.event.block.BlockDispenseEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.ShapelessRecipe;
-import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.scheduler.BukkitRunnable;
+
+import com.klin.holoItems.HoloItems;
+import com.klin.holoItems.Item;
+import com.klin.holoItems.interfaces.Dispensable;
+import com.klin.holoItems.interfaces.Placeable;
+
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
 
 public class SandPortal extends Item implements Dispensable, Placeable {
     public static final String name = "sandPortal";
-    private static final String[] types = new String[]{"RED_SAND", "GRAVEL",
-            "WHITE_CONCRETE_POWDER", "ORANGE_CONCRETE_POWDER", "MAGENTA_CONCRETE_POWDER",
-            "LIGHT_BLUE_CONCRETE_POWDER", "YELLOW_CONCRETE_POWDER", "LIME_CONCRETE_POWDER",
-            "PINK_CONCRETE_POWDER", "GRAY_CONCRETE_POWDER", "LIGHT_GRAY_CONCRETE_POWDER",
-            "CYAN_CONCRETE_POWDER", "PURPLE_CONCRETE_POWDER", "BLUE_CONCRETE_POWDER",
-            "BROWN_CONCRETE_POWDER", "GREEN_CONCRETE_POWDER", "RED_CONCRETE_POWDER",
-            "BLACK_CONCRETE_POWDER", "SAND"};
+    private static final Material[] types = new Material[] { Material.SAND, Material.RED_SAND, Material.GRAVEL,
+            Material.WHITE_CONCRETE_POWDER, Material.ORANGE_CONCRETE_POWDER, Material.MAGENTA_CONCRETE_POWDER,
+            Material.LIGHT_BLUE_CONCRETE_POWDER, Material.YELLOW_CONCRETE_POWDER, Material.LIME_CONCRETE_POWDER,
+            Material.PINK_CONCRETE_POWDER, Material.GRAY_CONCRETE_POWDER, Material.LIGHT_GRAY_CONCRETE_POWDER,
+            Material.CYAN_CONCRETE_POWDER, Material.PURPLE_CONCRETE_POWDER, Material.BLUE_CONCRETE_POWDER,
+            Material.BROWN_CONCRETE_POWDER, Material.GREEN_CONCRETE_POWDER, Material.RED_CONCRETE_POWDER,
+            Material.BLACK_CONCRETE_POWDER };
 
     private static final Material material = Material.SAND;
     private static final int quantity = 1;
-    private static final String lore =
-            "Dispense to create a physical block\n"+
+    private static final String lore = "Dispense to create a physical block\n" +
             "Can also be placed infinitely";
     private static final int durability = 0;
     public static final boolean stackable = true;
     private static final boolean shiny = false;
     public static final int cost = 19200;
 
-    public SandPortal(){
+    public SandPortal() {
         super(name, material, quantity, lore, durability, stackable, shiny, cost);
     }
 
-    public void registerRecipes(){
-        for(String type : types) {
-            Material material = Material.getMaterial(type);
-            item.setType(material);
-            String key = "";
-            String[] strings = type.toLowerCase().split("_");
-            for(String str : strings)
-                key += str.substring(0, 1).toUpperCase()+str.substring(1)+" ";
-            key += "Portal";
-            ItemMeta meta = item.getItemMeta();
-            meta.setDisplayName("§6"+key);
-            item.setItemMeta(meta);
-            key = key.substring(0, 1).toLowerCase()+
-                    key.substring(1).replace(" ", "");
-
-            ShapelessRecipe recipe = new ShapelessRecipe(
-                    new NamespacedKey(HoloItems.getInstance(), key), item);
+    @Override
+    public void registerRecipes() {
+        for (final var material : types) {
+            final var materialName = material.key().value().split("_");
+            final var builder = new StringBuilder();
+            for (final var s : materialName) {
+                builder.append(Character.toUpperCase(s.charAt(0))).append(s.substring(1)).append(' ');
+            }
+            builder.append("Portal");
+            final var displayName = builder.toString();
+            builder.setCharAt(0, Character.toLowerCase(builder.charAt(0)));
+            var i = 0;
+            for (final var s : materialName) {
+                i += s.length();
+                builder.deleteCharAt(i);
+            }
+            final var key = builder.toString();
+            final var itemStack = new ItemStack(material);
+            final var itemMeta = itemStack.getItemMeta();
+            itemMeta.displayName(Component.text(displayName, NamedTextColor.GOLD));
+            itemStack.setItemMeta(itemMeta);
+            final var recipe = new ShapelessRecipe(new NamespacedKey(HoloItems.getInstance(), key), itemStack);
             recipe.addIngredient(Material.NETHERITE_BLOCK);
             recipe.addIngredient(material);
             recipe.setGroup(name);
@@ -67,24 +71,24 @@ public class SandPortal extends Item implements Dispensable, Placeable {
         }
     }
 
-    public void ability(BlockDispenseEvent event){
-        Block block = event.getBlock();
-        BlockFace face = ((Dispenser) block.getBlockData()).getFacing();
-        Block place = block.getRelative(face);
+    @Override
+    public void ability(BlockDispenseEvent event) {
         event.setCancelled(true);
-        Material type = event.getItem().getType();
-        if(!place.getType().isAir()) {
-            BlockState container = place.getState();
-            if(container instanceof Container)
-                ((Container) container).getInventory().addItem(new ItemStack(type));
-            return;
+        final var block = event.getBlock();
+        final var type = event.getItem().getType();
+        final var facedBlock = block.getRelative(((Dispenser) block.getBlockData()).getFacing());
+        if (facedBlock.getType().isAir()) {
+            facedBlock.setType(type);
+        } else if (facedBlock.getState() instanceof Container container) {
+            container.getInventory().addItem(new ItemStack(type));
         }
-        place.setType(type);
     }
 
-    public void ability(BlockPlaceEvent event){
-        Block block = event.getBlockPlaced();
-        Material type = block.getType();
+    @Override
+    public void ability(BlockPlaceEvent event) {
+        // event is cancelled in Events.java
+        final var block = event.getBlockPlaced();
+        final var type = block.getType();
         new BukkitRunnable() {
             @Override
             public void run() {
